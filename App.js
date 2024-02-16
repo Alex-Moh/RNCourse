788c4a9839fb461d66d232dae79d703a33cf410a
Ajmal <ajmal2mohammad@gmail.com>
2024-02-15 20:17:05 -0500

    Added StatusBar

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
 import {LinearGradient} from 'expo-linear-gradient';
 import { useFonts } from 'expo-font'
 import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
 
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from './screens/GameScreen';
@@ -51,6 +52,8 @@ export default function App(){
     }
 
     return (
+    <>
+    <StatusBar style='light'/>
     <LinearGradient colors={[Colors.primary700, Colors.accent500]} style={styles.rootScreen}>
     <ImageBackground 
         source={require('./assets/images/backGround.png')} resizeMode='cover'
@@ -61,6 +64,7 @@ export default function App(){
         
     </ImageBackground>
     </LinearGradient>
+    </>
     );
 }
 
